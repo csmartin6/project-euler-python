baa3489c43d7bd7ee5df3c76575531fac90fe839
Chris Martin <csmartin6@gmail.com>
2014-11-06 23:41:22 -0500

    Cleanup of answer to problem 1

--- problem1.py
+++ problem1.py
@@ -1,5 +1,3 @@
+answer = sum([x for x in range(1000) if x%3 == 0 or x%5 == 0])
 
-multiples3or5 = [x for x in range(1000) if x%3 == 0 or x%5 == 0]
-product = sum(multiples3or5)
-
-print product
+print answer
