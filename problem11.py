f7d2c2dbe75a7fac336a6dd371c8f270f7419c6d
Chris Martin <csmartin6@gmail.com>
2014-12-02 10:59:36 -0500

    Moved the task of finding the max contiguous product into its own subroutine

--- problem11.py
+++ problem11.py
@@ -14,13 +14,15 @@ nums = np.array(arr)
 max_product_list = []
 # Check Rows
 
+def maxContigousProduct(arr,length):
+	products = [product(arr[j:(j+length)]) for j in range(len(arr)-length+1)]
+	return max(products)
+
 def maxProductInRow(arr):
 	max_product = 0
 	for row in arr: 
-		products = [product(row[j:(j+max_length)]) for j in range(len(row)-max_length+1)]
-		row_max  = max(products)
-		if row_max > max_product:
-			max_product = row_max
+		row_max  = maxContigousProduct(row,max_length)
+		max_product  = max(row_max,max_product)
 	return max_product
 
 def maxProductOnDiagonal(arr):
@@ -28,10 +30,8 @@ def maxProductOnDiagonal(arr):
 	max_product = 0;
 	for k in range(-m+max_length,n-max_length):
 		diag = np.diagonal(arr,k)
-		products = [product(diag[j:(j+max_length)]) for j in range(len(diag)-max_length+1)]
-		diag_max  = max(products)
-		if diag_max > max_product:
-			max_product = diag_max
+		diag_max = maxContigousProduct(diag,max_length)
+		max_product  = max(diag_max,max_product)
 
 	return max_product
 
@@ -40,7 +40,6 @@ maxInCol = maxProductInRow(nums.T)
 maxInDiag = maxProductOnDiagonal(nums)
 maxInDiagRot = maxProductOnDiagonal(np.rot90(nums))
 max_product = max([maxInRow,maxInCol,maxInDiag,maxInDiagRot])
-
 
 print max_product
 
