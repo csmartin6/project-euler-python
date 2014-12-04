6418288b2420a075156fa1d81e0dcec1f5f7e020
Chris Martin <csmartin6@gmail.com>
2014-12-04 10:16:18 -0500

    Problem 12

--- projectEulerUtils.py
+++ projectEulerUtils.py
@@ -1,4 +1,5 @@
 import math
+from collections import Counter
 
 def isPrime(N):
 	if N%2 == 0: 
@@ -19,7 +20,7 @@ def smallestPrimeFactor(N):
 	return N
 
 def primeFactors(N):
-
+	if N==1: return [1]
 	factors = []
 
 	while (N>1):
@@ -32,3 +33,13 @@ def primeFactors(N):
 
 def product(arr):
 	return reduce(lambda x,y:x*y,arr)
+
+
+def findDivisors(N):
+	factorPairs  = [(x, N/x) for x in range(1,int(math.sqrt(N))+1) if N % x == 0]
+	return [factor for pair in factorPairs for factor in pair]
+
+def countDivisors(N):
+	prime_factors = Counter(primeFactors(N))
+	possible_factors = [count+1 for factor, count in prime_factors.iteritems()]
+	return product(possible_factors)
