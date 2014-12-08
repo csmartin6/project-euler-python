c904423851ebe0f79db8fb2bd2703833b2fe8080
Chris Martin <csmartin6@gmail.com>
2014-12-07 22:39:33 -0500

    First crack at problem 13

---
+++ problem13.py
@@ -0,0 +1,28 @@
+from itertools import izip_longest
+
+def addNumArray(arr1,arr2):
+	arr = izip_longest(reversed(arr1),reversed(arr2), fillvalue=0)
+	result = []
+	carry = 0
+	for x,y in arr:
+		z = x + y + carry 
+		result.append(z%10)
+		carry = 1 if z >= 10 else 0
+	if carry == 1:
+		result.append(carry)
+	result.reverse()
+	return result
+
+arr = []
+max_length = 4
+with open('problem13_input.txt', 'r') as file:
+	for line in file:
+		current_line = line.rstrip('\n')
+		arr.append([int(x) for x in current_line])
+
+
+
+z = reduce(lambda x,y: addNumArray(x,y),arr)
+
+print z[0:10]
+
