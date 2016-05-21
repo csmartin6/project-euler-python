b3649fd0b6aa7502563609a8a4df3954932519be
Chris Martin <cmartin@palantir.com>
2016-05-21 12:45:01 -0500

    Problem 41

--- project_euler/problem_041.py
+++ project_euler/problem_041.py
@@ -5,11 +5,9 @@ import utils
 
 def is_pandigital(n):
     ns = str(n)
-    print ns
-    ns_set = set(ns[:])
-    print ns_set
+    ns_set = set(ns)
     for i in range(1,len(ns)+1):
-        if i not in ns_set:
+        if str(i) not in ns_set:
             return False
 
     return True
@@ -18,16 +16,15 @@ def is_pandigital(n):
 
 def problem_041():
     max_prime = 0;
-    for x in utils.prime_sieve(654321):
+    for x in utils.prime_sieve(87654322):
         if is_pandigital(x):
             max_prime = x
 
     return max_prime
 
 def main():
-    print is_pandigital(4321)
-    #print "Problem 41"
-    #print "Answer: " + str(problem_041())
+    print "Problem 41"
+    print "Answer: " + str(problem_041())
 
 
 
